5b044bbcb6cbe26f99dfc73a56fcc63270184ddb
958245365 <958245365@qq.com>
2020-12-23 22:24:51 +0800

    新增界面

---
+++ app/src/main/java/com/diwaves/news/mvp/presenter/YuCeCommentPresenter.kt
@@ -0,0 +1,48 @@
+package com.diwaves.news.mvp.presenter
+
+import android.app.Application
+
+import com.jess.arms.integration.AppManager
+import com.jess.arms.di.scope.ActivityScope
+import com.jess.arms.mvp.BasePresenter
+import com.jess.arms.http.imageloader.ImageLoader
+import me.jessyan.rxerrorhandler.core.RxErrorHandler
+import javax.inject.Inject
+
+import com.diwaves.news.mvp.contract.YuCeCommentContract
+
+
+/**
+ * ================================================
+ * Description:
+ * <p>
+ * Created by MVPArmsTemplate on 12/23/2020 22:22
+ * <a href="mailto:jess.yan.effort@gmail.com">Contact me</a>
+ * <a href="https://github.com/JessYanCoding">Follow me</a>
+ * <a href="https://github.com/JessYanCoding/MVPArms">Star me</a>
+ * <a href="https://github.com/JessYanCoding/MVPArms/wiki">See me</a>
+ * <a href="https://github.com/JessYanCoding/MVPArmsTemplate">模版请保持更新</a>
+ * ================================================
+ */
+@ActivityScope
+class YuCeCommentPresenter
+@Inject
+constructor(model: YuCeCommentContract.Model, rootView: YuCeCommentContract.View) :
+    BasePresenter<YuCeCommentContract.Model, YuCeCommentContract.View>(model, rootView) {
+    @Inject
+    lateinit var mErrorHandler: RxErrorHandler
+
+    @Inject
+    lateinit var mApplication: Application
+
+    @Inject
+    lateinit var mImageLoader: ImageLoader
+
+    @Inject
+    lateinit var mAppManager: AppManager
+
+
+    override fun onDestroy() {
+        super.onDestroy();
+    }
+}
---
+++ app/src/main/java/com/diwaves/news/mvp/ui/activity/YuCeCommentActivity.kt
@@ -0,0 +1,84 @@
+package com.diwaves.news.mvp.ui.activity
+
+import android.content.Intent
+import android.os.Bundle
+
+import com.jess.arms.base.BaseActivity
+import com.jess.arms.di.component.AppComponent
+import com.jess.arms.utils.ArmsUtils
+
+import com.diwaves.news.di.component.DaggerYuCeCommentComponent
+import com.diwaves.news.di.module.YuCeCommentModule
+import com.diwaves.news.mvp.contract.YuCeCommentContract
+import com.diwaves.news.mvp.presenter.YuCeCommentPresenter
+
+import com.diwaves.news.R
+
+
+/**
+ * ================================================
+ * Description:
+ * <p>
+ * Created by MVPArmsTemplate on 12/23/2020 22:22
+ * <a href="mailto:jess.yan.effort@gmail.com">Contact me</a>
+ * <a href="https://github.com/JessYanCoding">Follow me</a>
+ * <a href="https://github.com/JessYanCoding/MVPArms">Star me</a>
+ * <a href="https://github.com/JessYanCoding/MVPArms/wiki">See me</a>
+ * <a href="https://github.com/JessYanCoding/MVPArmsTemplate">模版请保持更新</a>
+ * ================================================
+ */
+/**
+ * 如果没presenter
+ * 你可以这样写
+ *
+ * @ActivityScope(請注意命名空間) class NullObjectPresenterByActivity
+ * @Inject constructor() : IPresenter {
+ * override fun onStart() {
+ * }
+ *
+ * override fun onDestroy() {
+ * }
+ * }
+ */
+class YuCeCommentActivity : BaseActivity<YuCeCommentPresenter>(), YuCeCommentContract.View {
+
+    override fun setupActivityComponent(appComponent: AppComponent) {
+        DaggerYuCeCommentComponent //如找不到该类,请编译一下项目
+            .builder()
+            .appComponent(appComponent)
+            .yuCeCommentModule(YuCeCommentModule(this))
+            .build()
+            .inject(this)
+    }
+
+
+    override fun initView(savedInstanceState: Bundle?): Int {
+        return R.layout.activity_yu_ce_comment //如果你不需要框架帮你设置 setContentView(id) 需要自行设置,请返回 0
+    }
+
+
+    override fun initData(savedInstanceState: Bundle?) {
+
+    }
+
+
+    override fun showLoading() {
+
+    }
+
+    override fun hideLoading() {
+
+    }
+
+    override fun showMessage(message: String) {
+        ArmsUtils.snackbarText(message)
+    }
+
+    override fun launchActivity(intent: Intent) {
+        ArmsUtils.startActivity(intent)
+    }
+
+    override fun killMyself() {
+        finish()
+    }
+}
